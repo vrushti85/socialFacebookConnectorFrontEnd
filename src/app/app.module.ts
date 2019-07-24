a39b1e4a996b206f77278cb7e6ee15da85226c6d
vrushti patel <vrushti.patel.sa@gmail.com>
2019-07-24 18:29:13 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,51 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import {
+  SocialLoginModule,
+  AuthServiceConfig,
+  GoogleLoginProvider,
+  FacebookLoginProvider
+} from "angular-6-social-login";
+
+export function getAuthServiceConfigs() {
+  let config = new AuthServiceConfig(
+    [
+      {
+        id: FacebookLoginProvider.PROVIDER_ID,
+        provider: new FacebookLoginProvider("2392721984317375")
+      },
+      {
+        id: GoogleLoginProvider.PROVIDER_ID,
+        provider: new GoogleLoginProvider("1030776932770-3e8ef1h7duavfbkmkn7dh6c98clc3ubl.apps.googleusercontent.com")
+      }
+    ]
+  );
+  return config;
+}
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFontAwesomeModule,
+    HttpClientModule,
+    SocialLoginModule
+  ],
+  providers: [{
+    provide: AuthServiceConfig,
+    useFactory: getAuthServiceConfigs
+  }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+import {
+  AuthService,
+  FacebookLoginProvider,
+  GoogleLoginProvider
+} from 'angular-6-social-login';
+import { HttpClient } from '@angular/common/http'
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private socialAuthService: AuthService, private http: HttpClient) { }
+  
+  private loggedIn: boolean;
+  ngOnInit() {
+    this.socialAuthService.authState.subscribe((user) => {
+      this.loggedIn = (user != null);
+    });
+  }
+
+  public socialSignIn(socialPlatform: string) {
+    let socialPlatformProvider;
+    if (socialPlatform == "facebook") {
+      socialPlatformProvider = FacebookLoginProvider.PROVIDER_ID;
+    } else if (socialPlatform == "google") {
+      socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
+    }
+    this.socialAuthService.signIn(socialPlatformProvider).then(
+      (userData) => {
+        console.log(socialPlatform + " sign in data : ", userData, typeof (userData));
+        this.http.post("http://localhost:2000/api/resData", userData)
+          .subscribe(res => {
+            console.log(res);
+          });
+      });
+  }
+}
